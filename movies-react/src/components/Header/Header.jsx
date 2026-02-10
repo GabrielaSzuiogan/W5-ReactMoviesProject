0b43d30f86cf48f8a8daa80177a5bc8c812bde55
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-02-10 14:29:57 +0200

    design: remade header

--- movies-react/src/components/Header/Header.jsx
+++ movies-react/src/components/Header/Header.jsx
@@ -2,9 +2,21 @@ import "./Header.css";
 import headerLogo from "../../assets/popcorn.png";
 function Header() {
   return (
-    <div className="headerWrapper">
-      <img src={headerLogo} className="headerLogo" alt="Header Logo" />
-      <h1 className="headerTitle">Popcorn</h1>
+    <div className="header-wrapper">
+      <div className="header-left">
+        <div className="logo-title">
+          <img src={headerLogo} className="logo" alt="Header Logo" />
+          <a href="#" className="header-title">
+            Popcorn
+          </a>
+        </div>
+        <a href="#">Watch List</a>
+        <a href="#">Favourites</a>
+      </div>
+      <div className="header-right">
+        <div className="drop-down">DropMenu</div>
+        <div className="search-bar">Search Bar</div>
+      </div>
     </div>
   );
 }
