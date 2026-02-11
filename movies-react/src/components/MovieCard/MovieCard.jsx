551c7b37ad10bcf1ff78e3d0348bef8674cbfb3c
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-02-11 10:53:23 +0200

    design: added add and favorite button

--- movies-react/src/components/MovieCard/MovieCard.jsx
+++ movies-react/src/components/MovieCard/MovieCard.jsx
@@ -4,16 +4,27 @@ function MovieCard({ title, image, genre, rating }) {
   return (
     <div className="movie-card">
       <img
-        src={`src//assets/images/${image}`}
+        src={`src/assets/images/${image}`}
         alt={title}
         className="movie-img"
       />
+
       <div className="movie-body">
-        <h3 className="movie-title">{title}</h3>
-        <div className="movie-details">
-          <span className="movie-genre">{genre}</span>
-          <span className="movie-rating">★ {rating}</span>
+        <button className="action-btn add-btn" title="Add to Watchlist">
+          +
+        </button>
+
+        <div className="movie-info">
+          <h3 className="movie-title">{title}</h3>
+          <div className="movie-details">
+            <span className="movie-genre">{genre}</span>
+            <span className="movie-rating">★ {rating}</span>
+          </div>
         </div>
+
+        <button className="action-btn fav-btn" title="Add to Favourites">
+          ♥
+        </button>
       </div>
     </div>
   );
