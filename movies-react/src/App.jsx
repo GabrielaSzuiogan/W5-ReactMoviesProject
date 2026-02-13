e4ef44a76646f964adbed44f10f1f45c679ef080
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-02-13 08:45:32 +0200

    func: save to local storage movie added to watch list, favorite and tab selected

--- movies-react/src/App.jsx
+++ movies-react/src/App.jsx
@@ -1,16 +1,34 @@
 import Header from "./components/Header/Header";
 import MovieList from "./components/MainContent/MovieList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import moviesData from "./movies.json";
 
 function App() {
   const [searchGenre, setSearchGenre] = useState("");
   const [genre, setGenre] = useState("");
 
-  const [watchlist, setWatchlist] = useState([]);
-  const [favorites, setFavorites] = useState([]);
+  const [watchlist, setWatchlist] = useState(
+    () => JSON.parse(localStorage.getItem("watchListLS")) || [],
+  );
+  const [favorites, setFavorites] = useState(
+    () => JSON.parse(localStorage.getItem("favoritesLS")) || [],
+  );
 
-  const [currentView, setCurrentView] = useState("home");
+  const [currentView, setCurrentView] = useState(
+    () => JSON.parse(localStorage.getItem("viewLS")) || "home",
+  );
+
+  useEffect(() => {
+    localStorage.setItem("watchListLS", JSON.stringify(watchlist));
+  }, [watchlist]);
+
+  useEffect(() => {
+    localStorage.setItem("favoritesLS", JSON.stringify(favorites));
+  }, [favorites]);
+
+  useEffect(() => {
+    localStorage.setItem("viewLS", JSON.stringify(currentView));
+  }, [currentView]);
 
   const handleToggleWatchlist = (movie) => {
     setWatchlist((prev) => {
